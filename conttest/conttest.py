134f12b138285441d3f66dc4ee4ce53cc94468f4
Angel ILIEV <a.v.iliev13@gmail.com>
2020-03-13 08:19:17 +0200

    Moved logic to original file. Added argparser

--- conttest/conttest.py
+++ conttest/conttest.py
@@ -6,16 +6,19 @@ Usage:
   conttest <cmd> ...
 
 """
-
+import argparse
 import hashlib
 import os.path
 import re
 import subprocess
-import sys
 import time
 
 HASHES = "hashes"
 TIMES = "times"
+
+IGNORE_PREFIXES = (".", "#")
+IGNORE_EXTENSIONS = ("pyc", "pyo", "_flymake.py")
+IGNORE_DIRS = (".git", ".hg", ".svn")
 
 
 def include_file_in_checks(path, excludes):
@@ -24,9 +27,6 @@ def include_file_in_checks(path, excludes):
     file has an undesired prefix, an undesired file extension, or
     lives in an undesired directory.
     """
-    IGNORE_PREFIXES = (".", "#")
-    IGNORE_EXTENSIONS = ("pyc", "pyo", "_flymake.py")
-    IGNORE_DIRS = (".git", ".hg", ".svn")
 
     basename = os.path.basename(path)
     for p in IGNORE_PREFIXES:
@@ -62,7 +62,7 @@ def getstate(full_path, method):
         return None
 
 
-def walk(top, method, filestates={}, excludes=[]):
+def walk(top, method, filestates, excludes):
     """
     Walk directory recursively, storing a hash value for any
     non-excluded file; return a dictionary for all such files.
@@ -80,7 +80,7 @@ def walk(top, method, filestates={}, excludes=[]):
 def get_exclude_patterns_from_file(path):
     if not os.path.exists(path):
         return []
-    with file(path) as f:
+    with open(path, "r", encoding="utf-8") as f:
         return [s.strip() for s in f.read().split() if s.strip() != ""]
 
 
@@ -124,20 +124,50 @@ def watch_dir(dir_, callback, method=HASHES):
         time.sleep(0.3)
 
 
-def do_command_on_update(cmd):
+callbacks = {
+    "black": "py -3 -m black {changed_files}",
+    "flake8": "py -3 -m flake8 {changed_files}",
+    "pytest": "py -3 -m pytest tests/",
+}
+
+
+def run_tasks(changed_files):
+    for name, cmd in callbacks.items():
+        if "{changed_files}" in cmd:
+            cmd = cmd.format(changed_files=" ".join(changed_files))
+
+        print(f"Running command `{cmd}`")
+        subprocess.call(cmd, shell=True)
+
+
+def run_callback_on_update(callback):
     try:
-        watch_dir(".", lambda changes: subprocess.call(cmd, shell=True), method=TIMES)
+        watch_dir(".", callback, method=TIMES)
     except KeyboardInterrupt:
-        print
+        print()
 
 
-def main():
-    cmd = " ".join(sys.argv[1:])
-    if cmd:
-        do_command_on_update(cmd)
-    else:
-        print("Usage: %s command args ..." % __file__)
+def main(directory):
+    if not os.path.isdir(directory):
+        print(f"Usage: {__file__} directory")
+        return
+
+    os.chdir(directory)
+    print(f"Started watching directory: `{directory}`")
+    run_callback_on_update(run_tasks)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Watch a given folder for changes. If a file is changed, run the provided commands for it."
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        type=str,
+        required=True,
+        help="Directory to be watched for changes.",
+    )
+
+    args = parser.parse_args()
+    main(directory=args.directory)
